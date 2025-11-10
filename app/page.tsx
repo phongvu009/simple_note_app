b686ce7ea24ca3f4aba4501d182787779b518ae1
pv_pial <kimphong90@hotmail.com>
2025-11-10 14:56:25 -0600

    Add feature section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 
 import { HeroHeader } from "@/components/header";
 import HeroSection from "@/components/hero-section";
+import Features from "@/components/features"
 
 export default function Home() {
   return (
     <main>
       <HeroHeader />
       <HeroSection />
+      <Features />
     </main>
   )
 }
