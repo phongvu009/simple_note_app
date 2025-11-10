c30cb26f70d9664f933947fc9a320679a0c19954
pv_pial <kimphong90@hotmail.com>
2025-11-10 15:02:09 -0600

    Add Footer Section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { HeroHeader } from "@/components/header";
 import HeroSection from "@/components/hero-section";
 import Features from "@/components/features"
 import CallToAction from "@/components/call-to-action";
+import FooterSection from "@/components/footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <Features />
       <CallToAction />
+      <FooterSection />
     </main>
   )
 }
