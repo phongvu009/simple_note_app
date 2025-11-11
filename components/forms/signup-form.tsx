9f378bf3c7d85921e436773bb223b48b4b30e8fb
pv_pial <kimphong90@hotmail.com>
2025-11-11 14:50:12 -0600

    Add SignUp form

--- components/forms/signup-form.tsx
+++ components/forms/signup-form.tsx
@@ -2,7 +2,28 @@
 
 import {cn } from "@/lib/utils"
 
+import {z} from "zod"
+import {useForm} from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
+
+//form schema
+const formSchema = z.object({
+    email: z.email(),
+    username: z.string().min(3).max(50),
+    password : z.string().min(8)
+
+})
+
 export function SignupForm({className, ...props}:React.ComponentProps<"div">){
+    //Define form using react hook form
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues : {
+            email: "",
+            username: "",
+            password: ""
+        }
+    })
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
 
